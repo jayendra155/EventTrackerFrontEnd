48efae63ffb04f1d69c407dac457ee28a04bc74f
jayendra155 <jayendra155@hotmail.com>
2016-05-13 07:24:34 +0530

    divs added

--- js/Controllers/Registrationcontroller.js
+++ js/Controllers/Registrationcontroller.js
@@ -12,12 +12,37 @@ var RegistrationController=function($scope,$http,$location,$timeout){
 	$scope.appName="Event Tracker";
 	console.log($scope);
 	$scope.register=function(){
-		$http.get("http://localhost:8081/EventTracker/register.html?username="+$scope.user.username+"&password="+$scope.user.password)
+		$http.get("http://localhost:8082/EventTracker/register.html?username="+$scope.user.username+"&password="+$scope.user.password)
 		.then(function success(result){
 			console.log("User registered")
 		},function failure(){
 			console.log("Registration failed")
 		});
+	}
+	
+	$scope.checkPassword=function(){
+		var passwordElement=document.getElementById('password');
+	    var password=passwordElement.value;
+	    if(password.length>=8 && password.length<=16)
+	    {
+	         if(/[A-Z]/.test(password)){
+	         if(/[0-9]/.test(password) ){
+	             if( /[!@#\$%^&*()_]/.test(password))
+	               {
+	            	 	$scope.passwordStrength='\&\#10003 ';
+	                    var cP=document.getElementById('confirmPassword');
+	                    cP.disabled=false;
+	               }
+	         }
+	     }
+	         
+	    }
+	    else
+       	 {
+	    	$scope.passwordStrength="Invalid";$scope.passwordStrength="Invalid";
+	    	var cP=document.getElementById('confirmPassword');
+            cP.disabled=true;
+       	 }
 	}
 };
 app.controller("RegistrationController",RegistrationController);
